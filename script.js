2228183ac5efcaebb9a0f405f162030eddbea664
siddhesh-ss <siddheshshelar32@gmail.com>
2026-08-04 07:26:36 +0000

    Fixed the bug of html change from input boxes with replacing innerHTML to textcontent. Added sorting function with multiple options.

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let bookList = document.querySelector("#book-list");
 
 let tableBody = document.querySelector("#table-body");
 
+let nextOrder = 0;
+let sorting = document.querySelector("#sorting");
+
 const myLibrary = [];
 
 function Book(title, author, pageno, isRead) {
@@ -16,6 +19,7 @@ function Book(title, author, pageno, isRead) {
     this.author = author;
     this.pageno = pageno;
     this.isRead = isRead;
+    this.order = nextOrder++;
 }
 
 function addBookToLibrary(title, author, pageno, isRead) {
@@ -29,6 +33,18 @@ submitBtn.addEventListener("click", (e) => {
         alert("Please fill all inputs");
         return;
     };
+    if(String(title.value).length > 35) {
+        alert("Sorry. Title length is too long!");
+        return;
+    }
+    if(String(author.value).length > 35) {
+        alert("Sorry. Author length is too long!");
+        return;
+    }
+    if(Number(pages.value) > 99999) {
+        alert("That's too much pages, can you tear up some?");
+        return;
+    }
     addBookToLibrary(title.value, author.value, pages.value, (read.value === 'read' ? true : false));
     clearForm();
 });
@@ -67,16 +83,46 @@ function changeStatus(bookId) {
 function display() {
     tableBody.innerHTML = "";
     for(const book of myLibrary) {
-        let row = ` <tr id="${book.id}">
-                    <td>${book.title}</td>
-                    <td>${book.author}</td>
-                    <td>${book.pageno}</td>
-                    <td><button class="read-btn ${(book.isRead ? "read" : "not-read")}" id="read-btn">${(book.isRead ? "Read" : "Not Read")}</button></td>
-                    <td><button class="delete-btn" id="delete-btn">DELETE</button></td>
-                    </tr>`;
-        tableBody.insertAdjacentHTML("afterbegin", row);
+        const tr = document.createElement("tr");
+        tr.id = book.id;
+
+        const titleTd = document.createElement("td");
+        titleTd.textContent = book.title;
+
+        const authorTd = document.createElement("td");
+        authorTd.textContent = book.author;
+
+        const pageTd = document.createElement("td");
+        pageTd.textContent = book.pageno;
+
+        const readTd = document.createElement("td");
+        const readBtn = document.createElement("button");
+        readBtn.className = `read-btn ${(book.isRead ? "read" : "not-read")}`
+        readBtn.textContent = `${(book.isRead ? "Read" : "Not Read")}`;
+        readTd.appendChild(readBtn);
+
+        const deleteTd = document.createElement("td");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.className = "delete-btn";
+        deleteBtn.textContent = "DELETE";
+
+        tr.append(titleTd,authorTd, pageTd, readTd, deleteTd);
+        tableBody.prepend(tr);
     }
 }
 
 addBookToLibrary("Game of Thrones", "George R. R. Martin", 1016, true);
 addBookToLibrary("Harry Potter", "J. K. Rowling", 607, false);
+
+sorting.addEventListener("change", () => {
+    if(sorting.value === "oldest") myLibrary.sort((a, b) => b.order - a.order);
+    else if(sorting.value === "newest") myLibrary.sort((a, b) => a.order - b.order);
+    else if(sorting.value === "pages-dec") myLibrary.sort((a, b) => a.pageno - b.pageno);
+    else if(sorting.value === "pages-inc") myLibrary.sort((a, b) => b.pageno - a.pageno);
+    else if(sorting.value === "read-stat") myLibrary.sort((a, b) => {
+        if(a.isRead === true && b.isRead === false) return 1;
+        if(a.isRead === false && b.isRead === true) return -1;
+        return 0;
+    });
+    display();
+})
